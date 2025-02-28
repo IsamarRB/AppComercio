811037ff022834fd2ccf353c4e52fea4fba39aeb
IsaRB <romerobellorini@gmail.com>
2025-02-28 15:10:57 +0100

    Add firt line code to AppComercio/main

---
+++ src/main/java/FormacionJava/AppComercio.java
@@ -0,0 +1,8 @@
+package FormacionJava;
+
+public class AppComercio {
+    public static void main (String[] args) {
+
+        System.out.printf("Hello and welcome to AppComercio by IsaRB!");
+        }
+    }
