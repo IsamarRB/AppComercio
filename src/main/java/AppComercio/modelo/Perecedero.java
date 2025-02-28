e8c00304e44e56e26d9f5776fd7bcb03d7205d39
IsaRB <romerobellorini@gmail.com>
2025-02-28 15:25:59 +0100

    Add class Perecedero code

---
+++ src/main/java/AppComercio/modelo/Perecedero.java
@@ -0,0 +1,25 @@
+package AppComercio.modelo;
+
+public class Perecedero extends Producto{
+    private int diasCaducar;
+
+    public Perecedero(int codProducto, String nombre, double precio, int stock, int diasCaducar) {
+        super(codProducto, nombre, precio, stock);
+        this.diasCaducar = diasCaducar;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.printf("Días para caducar: %d\n", diasCaducar);
+    }
+
+    @Override
+    public void actualizarPrecio() {
+        if (diasCaducar <= 2) {
+            precio *= 0.7;
+        } else {
+            precio *= 0.9;
+        }
+    }
+}
--- src/main/java/AppComercio/modelo/Producto.java
+++ src/main/java/AppComercio/modelo/Producto.java
@@ -1,4 +1,29 @@
 package AppComercio.modelo;
 
-public class Producto {
+public abstract class Producto implements GestionProducto {
+    protected int codProducto;
+    protected String nombre;
+    protected double precio;
+    protected int stock;
+
+    public Producto(int codProducto, String nombre, double precio, int stock) {
+        this.codProducto = codProducto;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public void mostrarInfo() {
+        System.out.printf("Código: %d | Nombre: %s | Precio: %.2f | Stock: %d\n", codProducto, nombre, precio, stock);
+    }
+
+    public void incrementarStock(int cantidad) {
+        this.stock += cantidad;
+    }
+
+    public void disminuirStock() {
+        if (stock > 0) {
+            stock--;
+        }
+    }
 }
