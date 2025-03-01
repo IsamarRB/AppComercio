31f2e4dd1b5b721d695a9aea57718eea414b0a16
IsaRB <romerobellorini@gmail.com>
2025-03-01 12:16:29 +0100

    Add new atributos en Persona

--- src/main/java/AppComercio/cliente/Persona.java
+++ src/main/java/AppComercio/cliente/Persona.java
@@ -4,11 +4,15 @@ public class Persona {
     private String nomCompleto;
     private String dni;
     private String direccion;
+    private String email;
+    private int móvil;
 
     public Persona(String nomCompleto, String dni, String direccion) {
         this.nomCompleto = nomCompleto;
         this.dni = dni;
         this.direccion = direccion;
+        this.email = email;
+        this.móvil = móvil;
     }
 
     public String getNomCompleto() {
@@ -21,5 +25,13 @@ public class Persona {
 
     public String getDireccion() {
         return direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getMóvil() {
+        return móvil;
     }
 }
