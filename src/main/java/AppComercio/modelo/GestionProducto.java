a6b70c3c7154896bc2a52714465881c7bf75fdb3
IsaRB <romerobellorini@gmail.com>
2025-03-02 13:53:39 +0100

    Added JavaDoc- GestionProducto

--- src/main/java/AppComercio/modelo/GestionProducto.java
+++ src/main/java/AppComercio/modelo/GestionProducto.java
@@ -1,10 +1,29 @@
 package AppComercio.modelo;
 
+/**
+ * Interfaz que define las operaciones de gestión de productos en la aplicación de comercio.
+ */
 public interface GestionProducto {
-    // Método mostrarInfo
+
+    /**
+     * Muestra la información del producto.
+     */
     void mostrarInfo();
 
+    /**
+     * Incrementa el stock del producto en la cantidad especificada.
+     *
+     * @param cantidad la cantidad en la que se incrementará el stock
+     */
     void incrementarStock(int cantidad);
+
+    /**
+     * Disminuye el stock del producto en una unidad.
+     */
     void disminuirStock();
+
+    /**
+     * Actualiza el precio del producto.
+     */
     void actualizarPrecio();
 }
