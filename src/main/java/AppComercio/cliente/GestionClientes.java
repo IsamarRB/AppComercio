2988039ef9d2dadb612560d97f1721ea291b9750
IsaRB <romerobellorini@gmail.com>
2025-03-02 13:47:53 +0100

    Added JavaDoc de GestionClientes

--- src/main/java/AppComercio/cliente/GestionClientes.java
+++ src/main/java/AppComercio/cliente/GestionClientes.java
@@ -1,7 +1,24 @@
 package AppComercio.cliente;
 
+/**
+ * Interfaz que define la gestión de clientes en la aplicación de comercio.
+ * Proporciona métodos para obtener información del cliente,
+ * visualizar el carrito de compras y enviar un pedido.
+ */
 public interface GestionClientes {
+
+    /**
+     * Muestra la información del cliente.
+     */
     void infoCliente();
+
+    /**
+     * Muestra el contenido del carrito de compras del cliente.
+     */
     void mostrarCarrito();
+
+    /**
+     * Envía el pedido del cliente.
+     */
     void enviarPedido();
 }
